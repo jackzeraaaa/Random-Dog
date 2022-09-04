f9376a0fec514d918ebd60600bb8793df0609ee8
jackzeraaaa <hangoverbicha@gmail.com>
2022-09-04 17:06:37 -0300

    adicionado botao prefer cats

--- apifetch.js
+++ apifetch.js
@@ -1,12 +1,24 @@
+const img = document.querySelector("img")
+const loadingCircle = document.querySelector(".loading-circle")
+
 const generateDog = () => {
-    const img = document.querySelector("img")
-    const loadingCircle = document.querySelector(".loading-circle")
     loadingCircle.classList.remove("invisible");
 
     fetch("https://dog.ceo/api/breeds/image/random")
     .then(response => response.json())
     .then(data => {
         img.src = data.message;
+        loadingCircle.classList.add("invisible")
+    })
+}
+
+const generateCat = () => {
+    loadingCircle.classList.remove("invisible");
+
+    fetch("https://api.thecatapi.com/v1/images/search")
+    .then(response => response.json())
+    .then(data => {
+        img.src = data[0].url;
         loadingCircle.classList.add("invisible")
     })
 }
